5b0cc8597c2d6096a3b34cebba44e94ef33185e5
John Doe <111723388+simocoder@users.noreply.github.com>
2025-07-04 21:14:55 +1000

    edit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,6 +9,7 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID for Amazon Linux 2"
   # default     = "ami-0c55b159cbfafe1f0" # Update based on region
+  # default     = "ami-0e6874cbf738602e7" # Update based on region, was amazon linux 2???
   default     = "ami-0e6874cbf738602e7" # Update based on region
   
 }
